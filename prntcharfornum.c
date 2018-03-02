ce9796ead058eae162727cee8feb9c80374b1fe7
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-03-02 10:39:16 +0000

    Update prntcharfornum.c

--- prntcharfornum.c
+++ prntcharfornum.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-	int num;
-	scanf("%d",&num);
-            printf("%c",num+64);
+	int n;
+	scanf("%d",&n);
+            printf("%c",n+64);
 	// your code goes here
 	return 0;
 }
