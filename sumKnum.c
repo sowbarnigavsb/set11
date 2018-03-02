ae9043d825d22dfd0f997e5115ce0afa16fcbf5e
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-03-02 11:09:01 +0000

    Create sumKnum.c

---
+++ sumKnum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int k,sum=0;
+	scanf("%d",&k);
+	for(int i=1;i<=k;i++)
+	{
+	sum=sum+i;
+	}
+            printf("%d",sum);
+	// your code goes here
+	return 0;
+}
